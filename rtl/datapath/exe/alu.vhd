4c447214c512dec8ee44cfc1cd9d615d2615d157
fedefruttero <fruttero.fede@gmail.com>
2023-07-14 16:13:06 +0200

    asd

--- rtl/datapath/exe/alu.vhd
+++ rtl/datapath/exe/alu.vhd
@@ -1,76 +1,60 @@
-----------------------------------------------------------------------------------------------------------
--- Title      : ALU
--- Project    : DLX
-----------------------------------------------------------------------------------------------------------
--- File      : alu.vhd
--- Author    : Riccardo Fusari, Federico Fruttero, Gianluca Corso
------------------------------------------------------------------------------------------------------------
--- Description: This file contains the behavioral description in VHDL of
---              an alu
---
------------------------------------------------------------------------------------------------------------
 
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
-use IEEE.std_logic_arith.all;
-use WORK.alu_types.all;                 -- Custom data types package
+--use IEEE.std_logic_arith.all;
+use IEEE.numeric_std.all;
 
+entity alu is
+  generic (N : integer := 8);
+  port 	 ( FUNC: in std_logic_vector(3 downto 0); --4 bits for the 9 operations.
+           DATA1, DATA2: in std_logic_vector(N-1 downto 0);
+           OUTALU: out std_logic_vector(N-1 downto 0));
+end alu;
 
+architecture BEHAVIOR of alu is
 
-entity ALU is
-  generic (N : integer := 16);  -- Generic parameter N representing the size of input and output data
-  
-  port (
-              FUNC         : in  TYPE_OP;  
-              DATA1, DATA2 : in  std_logic_vector(N-1 downto 0);  
-              OUTALU       : out std_logic_vector(N-1 downto 0)
-       ); 
-
-end ALU;
-
-
-architecture BEHAVIOUR of ALU is
 begin
 
--- Process for performing arithmetic and logic operations
-  P_ALU : process (FUNC, DATA1, DATA2)
-
-    -- Declare variables for performing operations
-    variable half1 : std_logic_vector(N/2-1 downto 0);  -- Variable for storing the first half of input data1
-    variable half2 : std_logic_vector(N/2-1 downto 0);  -- Variable for storing the first half of input data2
-
+P_ALU: process (FUNC, DATA1, DATA2)
+  -- complete all the requested functions
+       variable half1: std_logic_vector(N/2-1 downto 0);
+       variable half2: std_logic_vector(N/2-1 downto 0);
   begin
-
-    half1 := DATA1(N/2-1 downto 0);     -- Extract the first half of data1
-    half2 := DATA2(N/2-1 downto 0);     -- Extract the first half of data2
-
-    -- Perform the operation based on the input function
+      half1 := DATA1(N/2-1 downto 0);
+      half2 := DATA2(N/2-1 downto 0);
+ 
+    
     case FUNC is
-      when ADD =>
-        OUTALU <= DATA1 + DATA2;  
-      when SUB =>
-        OUTALU <= DATA1 - DATA2;  
-      when MULT =>
-        OUTALU <= half1 * half2;  -- Multiply the first halves of data1 and data2 and store the result in output port
-      when BITAND =>
-        OUTALU <= DATA1 and DATA2;  
-      when BITOR =>
-        OUTALU <= DATA1 or DATA2;  
-      when BITXOR =>
-        OUTALU <= DATA1 xor DATA2;  
-      when FUNCLSL =>
-        OUTALU <= DATA1(N-2 downto 0) & '0';         -- Perform shift left using the concatenation operator &
-      when FUNCLSR =>
-        OUTALU <= '0' & DATA1(N-1 downto 1);         -- Perform shift right using the concatenation operator &
-      when FUNCRL =>
-        OUTALU <= DATA1(N-2 downto 0) & DATA1(N-1);  -- Perform rotate left using the concatenation operator &
-      when FUNCRR =>
-        OUTALU <= DATA1(0) & DATA1(N-1 downto 1);    -- Perform rotate right using the concatenation operator &
-      when others => null;
-    end case;
+ 	when "0000" =>
+               OUTALU <= DATA1 + DATA2;
+ 	when "0001"    =>
+               OUTALU <= DATA1 - DATA2;
+ 	when "0010"=>
+               OUTALU <= half1 * half2;
+ 	when "0011"=>
+               OUTALU <= DATA1 and DATA2; -- bitwise operations
+ 	when "0100"=>
+               OUTALU <= DATA1 or DATA2;
+  when "0101"=>
+               OUTALU <= DATA1 xor DATA2;
+	when "0110" => 
+              OUTALU <= std_logic_vector(unsigned(DATA1) SLL to_integer(unsigned(DATA2))); 
+	when "0111" => 
+              OUTALU <= std_logic_vector(unsigned(DATA1) SRL to_integer(unsigned(DATA2)));
+	when "1000" => 
+              OUTALU <= std_logic_vector(unsigned(DATA1) ROL to_integer(unsigned(DATA2)));
+	when "1001" => 
+              OUTALU <=  std_logic_vector(unsigned(DATA1) ROR to_integer(unsigned(DATA2)));
+
+	when others => NULL;
+
+    end case; 
   end process P_ALU;
 
-end BEHAVIOUR;
+end BEHAVIOR;
 
-
+configuration CFG_ALU_BEHAVIORAL of ALU is
+  for BEHAVIOR
+  end for;
+end CFG_ALU_BEHAVIORAL;
