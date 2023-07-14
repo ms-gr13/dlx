e34f53fbab4f3f2ae8adcbb153cb143df5663303
Gianluca Corso <s315100@studenti.polito.it>
2023-07-14 16:28:14 +0200

    alu

--- rtl/datapath/exe/alu.vhd
+++ rtl/datapath/exe/alu.vhd
@@ -4,10 +4,12 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 --use IEEE.std_logic_arith.all;
 use IEEE.numeric_std.all;
+use work.myTypes.all;
+
 
 entity alu is
-  generic (N : integer := 8);
-  port 	 ( FUNC: in std_logic_vector(3 downto 0); --4 bits for the 9 operations.
+  generic (N : integer := 32);
+  port 	 ( FUNC: in aluOp; --4 bits for the 9 operations.
            DATA1, DATA2: in std_logic_vector(N-1 downto 0);
            OUTALU: out std_logic_vector(N-1 downto 0));
 end alu;
@@ -26,26 +28,20 @@ P_ALU: process (FUNC, DATA1, DATA2)
  
     
     case FUNC is
- 	when "0000" =>
+ 	when ADDS =>
                OUTALU <= DATA1 + DATA2;
- 	when "0001"    =>
+ 	when SUBS =>
                OUTALU <= DATA1 - DATA2;
- 	when "0010"=>
-               OUTALU <= half1 * half2;
- 	when "0011"=>
+ 	when ANDS =>
                OUTALU <= DATA1 and DATA2; -- bitwise operations
- 	when "0100"=>
+ 	when ORS =>
                OUTALU <= DATA1 or DATA2;
-  when "0101"=>
+    when XORS =>
                OUTALU <= DATA1 xor DATA2;
-	when "0110" => 
+	when LLS =>
               OUTALU <= std_logic_vector(unsigned(DATA1) SLL to_integer(unsigned(DATA2))); 
-	when "0111" => 
+	when LSR =>
               OUTALU <= std_logic_vector(unsigned(DATA1) SRL to_integer(unsigned(DATA2)));
-	when "1000" => 
-              OUTALU <= std_logic_vector(unsigned(DATA1) ROL to_integer(unsigned(DATA2)));
-	when "1001" => 
-              OUTALU <=  std_logic_vector(unsigned(DATA1) ROR to_integer(unsigned(DATA2)));
 
 	when others => NULL;
 
