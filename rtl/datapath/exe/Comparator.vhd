2911b73c32ba690f6c9aefd58332c2a3445a7559
Gianluca Corso <s315100@studenti.polito.it>
2023-07-19 20:07:20 +0200

    comparator

--- rtl/datapath/exe/Comparator.vhd
+++ rtl/datapath/exe/Comparator.vhd
@@ -2,56 +2,56 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 use IEEE.std_logic_arith.all;
+use work.myTypes.all;
+
 
 entity Comparator is
-  generic (bits : integer :=32);
-  port (Cout: in std_logic;
-        sum: in std_logic_vector(bits-1 downto 0);
-        setneq: out std_logic;
-        setgr : out std_logic;
-        setlo : out std_logic
+  generic (bits : integer := 32);
+  port (Cout : in  std_logic;
+        EN   : in  std_logic;
+        func : in  aluOp;
+        sum  : in  std_logic_vector(bits-1 downto 0);
+        set  : out std_logic_vector(bits-1 downto 0)
         );
 end Comparator;
 
 
 architecture BEHAVIORAL of Comparator is
-  signal z : std_logic;
+  signal z       : std_logic;
   signal CoutInv : std_logic;
-  signal t : std_logic;
+  signal t       : std_logic;
+begin
+  process(Cout, sum, func)
   begin
-    process(Cout, sum)
-    begin
-      for i in bits-1 downto 0 loop
-        z <= not (sum(i) or '0');
-      end loop;
+    if EN = '1' then
+      case func is
+        when SGES =>
+          if Cout = '1' then
+            set <= (others => '1');
+          else
+            set <= (others => '0');
+          end if;
 
-      if z = '0' then
-        setneq <= '1';
+        when SLES =>
+          for i in bits-1 downto 0 loop
+            z <= not (sum(i) or '0');
+          end loop;
+          CoutInv <= not Cout;
+          t       <= CoutInv xor z;
+          if t = '1' then
+            set <= (others => '1');
+          else
+            set <= (others => '0');
+          end if;
+
+        when SNES =>
+          if z = '0' then
+            set <= (others => '1');
+          else
+            set <= (others => '0');
+          end if;
+        when others =>
+          set <= (others => '0');
       end if;
-
-      if Cout = '1' then
-        setgr <= '1';
-      end if;
-
-      CoutInv <= not Cout;
-
-      t <= CoutInv xor z;
-
-      if t = '1' then
-        setlo <= '1';
-      end if;
-
-  end process;
-
+    end process;
 end BEHAVIORAL;
-
-
-
-
-
-
-
-
-
-
-
